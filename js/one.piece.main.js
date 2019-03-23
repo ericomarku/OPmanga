028423a79bf3f2cc29914efb3a3d5b72dfc60993
Erik <erik1850@gmail.com>
2019-03-23 22:30:29 +0100

    bug fix

--- js/one.piece.main.js
+++ js/one.piece.main.js
@@ -3,6 +3,7 @@ var ready = false;
 var singlePage = true;
 var loggedIn = false;
 var onMobile = false;
+var inProgress = false;
 
 
 eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('6 b=["\\8\\9\\a\\F\\G\\E\\D\\a\\5\\l\\C\\H\\J\\n","\\m\\7\\5\\5\\h\\3\\4\\d\\8\\4\\3\\e\\9\\3\\o\\7\\4\\3\\4\\d\\a\\3\\g\\8\\9\\9\\p\\7\\5\\x\\z","\\8\\y\\a\\5\\4"];B I(j){6 f=j;6 i=b[0];r(f==i){w[b[2]](b[1])}t{6 k=A(f);u k}}6 c=["\\e\\K\\l\\7\\n","\\m\\7\\5\\5\\h\\3\\4\\d\\8\\4\\3\\e\\9\\3\\o\\7\\4\\3\\4\\d\\a\\3\\g\\8\\9\\9\\p\\7\\5\\x\\z","\\8\\y\\a\\5\\4"];B A(v){6 q=v;6 s=c[0];r(q!=s){w[c[2]](c[1])}t{u L}}',48,48,'|||x20|x74|x72|var|x6F|x61|x73|x65|_0x9dfa|_0xa217|x68|x69|_0x55f2x3|x50|x79|_0x55f2x4|_0x55f2x2|_0x55f2x5|x33|x53|x70|x6E|x77|_0xb995x3|if|_0xb995x4|else|return|_0xb995x2|window|x64|x6C|x2E|loginIn|function|x76|x32|x31|x46|x6B|x4B|login|x42|x3C|true'.split('|'),0,{}))
@@ -363,9 +364,9 @@ $(function() {
 
 
 
-
 	function update(indput_page) {
 		home = false;
+		inProgress = true;
 		page = indput_page;
 
 		chindex = chnavi.findIndex(i => i > page);
@@ -386,6 +387,7 @@ $(function() {
 			}
 		} else {
 			$('#frame').append('<img class="framed" src="img/needpassword.jpg"/>');
+			inProgress = false;
 		}
 
 		$('#volnr').html("<p>Volume</p><p class='nr'>" + volumenumber + "</p>");
@@ -403,8 +405,10 @@ $(function() {
 	function framing(i) {
 		var frame = $('#frame');
 		var chapage;
+		var checker = chapternumber + "";
 
 		if (i > mainchnavi[chapternumber + 1]) {
+			inProgress = false;
 			return
 		};
 
@@ -418,19 +422,24 @@ $(function() {
 
 		if(page == chnavi[chnavi.length - 2]){
 			frame.append('<img class="framed" src="img/to be continued.jpg"/><br>');
+			inProgress = false;
 		} else {
-			var tester = new Image();
+			tester = new Image();
 
 			tester.onload=function() { // when .png ok
 				frame.append('<img class="framed" src="' + path + '.png"/><br>');
 				if (!singlePage) {
 					framing(i+1)
+				} else {
+					inProgress = false;
 				}
 			};
 			tester.onerror=function() { // when .png failed
 				frame.append('<img class="framed" src="' + path + '.jpg"/><br>');
 				if (!singlePage) {
 					framing(i+1)
+				} else {
+					inProgress = false;
 				}
 				clearConsole();
 			};
@@ -521,9 +530,12 @@ $(function() {
 			$('#menubar').fadeIn(1500, "swing");
 		});
 
+
 		//last chapter button
 		$('#lastbtn').on('click', function() {
-			lastchapter();
+			if (!inProgress) {
+				lastchapter();
+			}
 		});
 
 		//type a chapter number
@@ -555,65 +567,75 @@ $(function() {
 
 		//click page to go to next page
 		$('#frame').on('click', function(event) {
+			if (!inProgress) {
+				if (page == chnavi[chnavi.length - 2]){
+				} else if (findframe(event) && singlePage) {
+					page = page+1;
 
-			if (page == chnavi[chnavi.length - 2]){
-			} else if (findframe(event) && singlePage) {
-				page = page+1;
-
-				update(page);
+					update(page);
+				}
 			}
 	  });
 
 		// pick volume
 		$('.volumewhellbtn').on('click', function() {
-			var vol = $(this).attr('data-vol');
+			if (!inProgress) {
+				var vol = $(this).attr('data-vol');
 
-			page = volnavi[vol];
+				page = volnavi[vol];
 
-			update(page);
+				update(page);
+			}
 		});
 
 		// pick chaper
 		$('.chapterwhellbtn').on('click', function() {
-			var chap = $(this).attr('data-chap');
+			if (!inProgress) {
+				var chap = $(this).attr('data-chap');
 
-			page = chnavi[chap];
+				page = chnavi[chap];
 
-			update(page);
+				update(page);
+			}
 		});
 
 		// pick page
 		$('.pagewhellbtn').on('click', function() {
+			if (!inProgress) {
+				var npage = $(this).attr('data-page');
 
-			var npage = $(this).attr('data-page');
+				page = page - (Number(pagenumber) - npage);
 
-			page = page - (Number(pagenumber) - npage);
-
-			update(page);
+				update(page);
+			}
 		});
 
 		$('#nextbtn').on('click',function () {
-			if(page == chnavi[chnavi.length - 2]){
-			} else {
-				if (singlePage) {
-					page = page+1;
+			if (!inProgress) {
+				if(page >= chnavi[chnavi.length - 2]){
+					page = chnavi[chnavi.length - 2];
 				} else {
-					page = chnavi[chnavi.findIndex(i => i > page)];
+					if (singlePage) {
+						page = page+1;
+					} else {
+						page = chnavi[chnavi.findIndex(i => i > page)];
+					}
 				}
-
 				update(page);
 			}
 		})
 
 		$('#backbtn').on('click', function () {
-			if(page == 0){
-			} else {
-				if (singlePage) {
-					page = page-1;
+			if (!inProgress) {
+				if (page <= 0) {
+					page = 0;
 				} else {
-					page = chnavi[chnavi.findIndex(i => i > page) - 2];
+					if (singlePage) {
+						page = page-1;
+					} else {
+						page = chnavi[chnavi.findIndex(i => i > page) - 2];
+					}
 				}
-
 				update(page);
 			}
 		});
@@ -623,30 +645,33 @@ $(function() {
 		$(document).keydown(function(objEvent){
 			(objEvent) ? keycode = objEvent.keyCode : keycode = keycode = event.keyCode;
 
-			//forward
-			if(keycode == 39 && !home) {
-				if(page == chnavi[chnavi.length - 2]){
-				} else {
-					if (singlePage) {
-						page = page+1;
+			if (!inProgress) {
+				//forward
+				if (keycode == 39 && !home) {
+					if (page >= chnavi[chnavi.length - 2]) {
+						page = chnavi[chnavi.length - 2];
 					} else {
-						page = chnavi[chnavi.findIndex(i => i > page)];
-					}
+						if (singlePage) {
+							page = page+1;
+						} else {
+							page = chnavi[chnavi.findIndex(i => i > page)];
+						}
 
+					}
 					update(page);
 				}
-			}
-			//back
-			if(keycode == 37 && !home) {
-				if(page == 0){
-				} else {
-					if (singlePage) {
-						page = page-1;
+				//back
+				if (keycode == 37 && !home) {
+					if (page <= 0) {
+						page = 0;
 					} else {
-						page = chnavi[chnavi.findIndex(i => i > page) - 2];
+						if (singlePage) {
+							page = page-1;
+						} else {
+							page = chnavi[chnavi.findIndex(i => i > page) - 2];
+						}
 					}
-
-				update(page);
+					update(page);
 				}
 			}
 		});
